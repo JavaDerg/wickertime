0724d8a53a587003ce7cfc684cb4859107721fde
JavaDerg <javaderg@pm.me>
2024-03-19 00:09:01 +0100

    vector like 2/3s but actually more like 3/5
    actually its more like 9/15

--- baum/src/inst.rs
+++ baum/src/inst.rs
@@ -1,7 +1,9 @@
 use crate::decl_types;
 use crate::NumType;
+use crate::VecType;
 
 // Good resource: https://developer.mozilla.org/en-US/docs/WebAssembly/Reference/
+// https://pengowray.github.io/wasm-ops/
 decl_types! {
     Sign => :enum {
         U,
@@ -10,6 +12,20 @@ decl_types! {
     Bits => :enum {
         _64,
         _32,
+    },
+    IShape => :enum {
+        I8X16,
+        I16X8,
+        I32X4,
+        I64X2,
+    },
+    FShape => :enum {
+        F32X4,
+        F64X2,
+    },
+    Shape => :enum {
+        I(IShape),
+        F(FShape),
     },
     Inst => :enum {
         Const(NumType),
@@ -86,6 +102,70 @@ decl_types! {
         IReinterpretF(Bits, Bits),
         FReinterpretI(Bits, Bits),
 
-        // to be done: all the othe ones :>
+        // Vector instrunctions
+        VConst(VecType),
+        VNot,
+        VAnd,
+        VAndNot,
+        VOr,
+        VXor,
+        VBitSelect,
+        VAnyTrue,
+
+        VI8X16Shuffle([u8; 16]),
+        VI8X16Swizzle,
+        VSplat(Shape),
+        VI8X16ExtractLane(Bits, u8),
+        VI16X8ExtractLane(Bits, u8),
+        VI32X4ExtractLine(u8),
+        VI64X2ExtractLine(u8),
+        VExtractLane(FShape, u8),
+        VReplaceLane(Shape, u8),
+        VI8X16Eq,
+        VI8X16Ne,
+        VI8X16Lt(Sign),
+        VI8X16Gt(Sign),
+        VI8X16Le(Sign),
+        VI8X16Ge(Sign),
+        VI16X8Eq,
+        VI16X8Ne,
+        VI16X8Lt(Sign),
+        VI16X8Gt(Sign),
+        VI16X8Le(Sign),
+        VI16X8Ge(Sign),
+        VI32X4Eq,
+        VI32X4Ne,
+        VI32X4Lt(Sign),
+        VI32X4Gt(Sign),
+        VI32X4Le(Sign),
+        VI32X4Ge(Sign),
+        VI64X2Eq,
+        VI64X2Ne,
+        VI64X2Lt,
+        VI64X2Gt,
+        VI64X2Le,
+        VI64X2Ge,
+        VFEq(FShape),
+        VFNe(FShape),
+        VFLt(FShape),
+        VFGt(FShape),
+        VFLe(FShape),
+        VFGe(FShape),
+        VIAbs(IShape),
+        VINeg(IShape),
+        VI8X16PopCnt,
+        VI16X8Q15MulRSatS,
+        VI32X4DotI16X8S,
+        VFAbs(FShape),
+        VFNeg(FShape),
+        VFSqrt(FShape),
+        VFCeil(FShape),
+        VFFloor(FShape),
+        VFTrunc(FShape),
+        VFNearest(FShape),
+        VIAllTrue(IShape),
+        VIBitMask(IShape),
+
+        // TODO: More stuff
     },
 }
